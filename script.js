56adbaf763f0729337ae07e3dffb68afe600f438
Vaibhav Vanage <65243646+Phantom-IN@users.noreply.github.com>
2023-04-05 00:35:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function init() {
     generateBar();
     document.getElementById("bs").removeAttribute('hidden');
     document.getElementById("qs").removeAttribute('hidden');
+    document.getElementById("hs").removeAttribute('hidden');
   }
 }
 
@@ -119,15 +120,67 @@ function quickSort(copy) {
   return swaps;
 }
 
+function heapSort(arr) {
+  const n = arr.length;
+  const swaps = [];
+
+  // Heapify the array
+  for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+    heapify(arr, n, i, swaps);
+  }
+
+  // Extract elements from the heap one by one
+  for (let i = n - 1; i >= 0; i--) {
+    // Move the root (maximum) element to the end of the array
+    [arr[0], arr[i]] = [arr[i], arr[0]];
+    swaps.push([0, i]);
+
+    // Heapify the remaining elements
+    heapify(arr, i, 0, swaps);
+  }
+
+  return swaps;
+}
+
+function heapify(arr, n, i, swaps) {
+  let largest = i;
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
+
+  // Find the largest element among i, left and right
+  if (left < n && arr[left] > arr[largest]) {
+    largest = left;
+  }
+  if (right < n && arr[right] > arr[largest]) {
+    largest = right;
+  }
+
+  if (largest !== i) {
+    // Swap the elements at i and largest
+    [arr[i], arr[largest]] = [arr[largest], arr[i]];
+    swaps.push([i, largest]);
+
+    // Recursively heapify the affected subtree
+    heapify(arr, n, largest, swaps);
+  }
+}
+
+function hs() {
+  const copy = [...arr];
+  const swap = heapSort(copy);
+  animate(swap);
+}
+
 function bs() {
-  const copy = [...arr]
-  const swap = bubbleSort(copy)
+  const copy = [...arr];
+  const swap = bubbleSort(copy);
   animate(swap);
 }
 
 function qs() {
-  const copy = [...arr]
+  const copy = [...arr];
   const swap = quickSort(copy);
+  // console.log(swap);
   animate(swap);
-  generateBar();
 }
+
